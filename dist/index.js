42b15e7d189eb887c73e979e95880c71ea8d872a
afeiship <1290657123@qq.com>
2020-11-20 17:49:56 +0800

    Release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Wrap url in dingtalk.
  * homepage: https://github.com/afeiship/next-to-dingtalk-url
  * version: 1.0.0
- * date: 2020-11-20 17:37:42
+ * date: 2020-11-20 17:49:51
  * license: MIT
  */
 
